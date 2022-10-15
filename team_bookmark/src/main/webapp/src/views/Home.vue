89f1ba94fff858380b73300cc2f4dd5c9207843b
KentaroYumita <m5251202@u-aizu.ac.jp>
2022-10-15 15:48:28 +0900

    CSSフレームワーク(Bulma)の導入

--- team_bookmark/src/main/webapp/src/views/Home.vue
+++ team_bookmark/src/main/webapp/src/views/Home.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="home">
     <h1>Home</h1>
+    <router-link to="/create">
+      <input class="button is-primary" type="button" value="部屋を作る" />
+    </router-link>
+    <router-link to="/join">
+      <input class="button is-primary" type="button" value="部屋を探す" />
+    </router-link>
   </div>
 </template>
 
