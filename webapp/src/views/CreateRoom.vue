203a0a9f9327195e84920be2ae1cd45d14f3de80
Makoto-Takeuchi <t419mako@gmail.com>
2022-10-19 10:59:20 +0900

    部屋作成画面作成。参加画面も少し加筆

--- webapp/src/views/CreateRoom.vue
+++ webapp/src/views/CreateRoom.vue
@@ -1,6 +1,22 @@
 <template>
   <div class="create_room">
     <h1>部屋の作成</h1>
+    <p>部屋の名前を決めてください！</p>
+    <div class="container is-max-desktop">
+      <div class="field has-addons">
+        <div class="control is-expanded">
+          <input class="input" type="text" placeholder="作成したい部屋の名前を入力してください">
+        </div>
+        <div class="control">
+          <router-link to="/join/bookmark">
+            <a class="button is-info">
+              作成
+            </a>
+          </router-link>
+        </div>
+      </div>
+    </div>
+    <p>エラー文はここ</p>
   </div>
 </template>
 
