86ec55ed731f40b5a54682e4fed086e792331bf2
KentaroYumita <m5251202@u-aizu.ac.jp>
2022-10-19 14:47:52 +0900

    cache clear

---
+++ webapp/src/views/BookmarkList.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="bookmark_list">
+    <h1>部屋名</h1>
+    <table class="table is-fullwidth">
+      <tr>
+        <th class="has-text-centered">タイトル</th>
+        <th class="has-text-centered">検索ワード</th>
+        <th class="has-text-centered">アイコン</th>
+        <th class="has-text-centered"></th>
+        <th class="has-text-centered"></th>
+        <th class="has-text-centered"></th>
+      </tr>
+      <tr>
+        <th class="has-text-centered">Vue.jsおすすめライブラリ１０選！</th>
+        <th class="has-text-centered">Vue.js ライブラリ 簡単</th>
+        <th class="has-text-centered">🐶</th>
+        <th class="has-text-centered">
+          <button class="button is-small is-primary" >
+          Info
+          </button>
+        </th>
+        <th class="has-text-centered">
+          <button class="button is-small is-primary" >
+          Edit
+          </button>
+        </th>
+        <th class="has-text-centered">
+          <button class="button is-small is-primary" >
+            Delete
+          </button>
+        </th>
+      </tr>
+      <tr>
+        <th class="has-text-centered">Vue.jsのすゝめ</th>
+        <th class="has-text-centered">Vue.js ライブラリ 簡単</th>
+        <th class="has-text-centered">🐱</th>
+        <th class="has-text-centered">
+          <button class="button is-small is-primary" >
+            Info
+          </button>
+        </th>
+        <th class="has-text-centered">
+          <button class="button is-small is-primary" >
+            Edit
+          </button>
+        </th>
+        <th class="has-text-centered">
+          <button class="button is-small is-primary" >
+            Delete
+          </button>
+        </th>
+      </tr>
+      <tr>
+        <th class="has-text-centered">Vue.jsの開発環境を構築</th>
+        <th class="has-text-centered">Vue.js 始め方</th>
+        <th class="has-text-centered">🦊</th>
+        <th class="has-text-centered">
+          <button class="button is-small is-primary" >
+            Info
+          </button>
+        </th>
+        <th class="has-text-centered">
+          <button class="button is-small is-primary" >
+            Edit
+          </button>
+        </th>
+        <th class="has-text-centered">
+          <button class="button is-small is-primary" >
+            Delete
+          </button>
+        </th>
+      </tr>
+      <tr>
+        <th class="has-text-centered">Quarkus 入門</th>
+        <th class="has-text-centered">Quarkus 始め方</th>
+        <th class="has-text-centered">🐠</th>
+        <th class="has-text-centered">
+          <button class="button is-small is-primary" >
+            Info
+          </button>
+        </th>
+        <th class="has-text-centered">
+          <button class="button is-small is-primary" >
+            Edit
+          </button>
+        </th>
+        <th class="has-text-centered">
+          <button class="button is-small is-primary" >
+            Delete
+          </button>
+        </th>
+      </tr>
+    </table>
+    <button class="button is-small is-primary" >
+      + add bookmark
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BookmarkList"
+}
+</script>
+
+<style scoped>
+
+</style>
