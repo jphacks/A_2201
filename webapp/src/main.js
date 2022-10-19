ac2c7670d90a7893de06c29b011d62be5a5e40d8
KentaroYumita <m5251202@u-aizu.ac.jp>
2022-10-19 14:46:12 +0900

    cache clear

--- webapp/src/main.js
+++
@@ -1,6 +0,0 @@
-import { createApp } from "vue";
-import router from "./router/index.ts";
-import store from "./store/index.ts";
-import App from "./App.vue";
-
-createApp(App).use(router).use(store).mount('#app');
