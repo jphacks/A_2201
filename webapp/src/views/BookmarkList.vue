d19a87d0256c3183ee42eedfd40f069805ac260f
s1260145 <s1260145@u-aizu.ac.jp>
2022-10-22 10:36:34 +0900

    ブックマークの削除

--- webapp/src/views/BookmarkList.vue
+++ webapp/src/views/BookmarkList.vue
@@ -3,36 +3,38 @@
     <h1>「{{ room.name }}」の部屋へようこそ</h1>
     <table class="table is-fullwidth">
       <thead>
-        <tr>
-          <th class="has-text-centered">タイトル</th>
-          <th class="has-text-centered">検索ワード</th>
-          <th class="has-text-centered">アイコン</th>
-          <th class="has-text-centered"></th>
-          <th class="has-text-centered"></th>
-          <th class="has-text-centered"></th>
-        </tr>
+      <tr>
+        <th class="has-text-centered">タイトル</th>
+        <th class="has-text-centered">検索ワード</th>
+        <th class="has-text-centered">アイコン</th>
+        <th class="has-text-centered"></th>
+        <th class="has-text-centered"></th>
+        <th class="has-text-centered"></th>
+      </tr>
       </thead>
       <tbody>
-        <tr v-for="bookmark in bookmarks" v-bind:key="bookmark.id">
-          <td>{{ bookmark.title }}</td>
-          <td>{{ bookmark.search_word }}</td>
-          <td> <img alt="animal" :src="require('@/assets/'+ bookmark.choice[0] + '-' + bookmark.choice[2] + '-' + bookmark.choice[1] +'.png')"> </td>
-          <td class="has-text-centered">
-            <button class="button is-small is-primary">
-              Info
-            </button>
-          </td>
-          <td class="has-text-centered">
-            <button class="button is-small is-primary">
-              Edit
-            </button>
-          </td>
-          <td class="has-text-centered">
-            <button class="button is-small is-primary" @click="showPopup(title)">
-              Delete
-            </button>
-          </td>
-        </tr>
+      <tr v-for="(bookmark, index) in bookmarks" v-bind:key="bookmark.id">
+        <td>{{ bookmark.title }}</td>
+        <td>{{ bookmark.search_word }}</td>
+        <td><img alt="animal"
+                 :src="require('@/assets/'+ bookmark.choice[0] + '-' + bookmark.choice[2] + '-' + bookmark.choice[1] +'.png')">
+        </td>
+        <td class="has-text-centered">
+          <button class="button is-small is-primary">
+            Info
+          </button>
+        </td>
+        <td class="has-text-centered">
+          <button class="button is-small is-primary">
+            Edit
+          </button>
+        </td>
+        <td class="has-text-centered">
+          <button class="button is-small is-primary" @click="showPopup(index)">
+            Delete
+          </button>
+        </td>
+      </tr>
       </tbody>
     </table>
 
@@ -77,6 +79,7 @@ export default {
       name: ''
     });
     let bookmarks = reactive([]);
+    let delBookmarkTitle = ref(null);
 
     onMounted(async () => {
       room.id = route.params.id;
@@ -96,27 +99,35 @@ export default {
 
     const title = ref("ここにタイトルがはいる");
 
-    const showPopup = (title) => {
+    const showPopup = (index) => {
+      delBookmarkTitle.value = bookmarks[index].title;
       let popup = document.getElementsByClassName("popup")[0];
       popup.style.visibility = "visible";
       let p = popup.getElementsByTagName("p")[0];
-      p.innerText = "登録されたブックマークを削除します。よろしいですか?\n\"" + title + "\"";
-      this.delComment = title;
+      p.innerText = "登録されたブックマークを削除します。よろしいですか?\n\"" + delBookmarkTitle.value + "\"";
       // PCでのスクロール禁止
       document.addEventListener("mousewheel", this.scroll_control, {passive: false});
       // スマホでのタッチ操作でのスクロール禁止
       document.addEventListener("touchmove", this.scroll_control, {passive: false});
     }
+
     const hidePopup = () => {
+      delBookmarkTitle.value = '';
       let popup = document.getElementsByClassName("popup")[0];
       popup.style.visibility = "hidden";
-      this.delComment = null;
       // PCでのスクロール禁止解除
       document.removeEventListener("mousewheel", this.scroll_control, {passive: false});
       // スマホでのタッチ操作でのスクロール禁止解除
       document.removeEventListener('touchmove', this.scroll_control, {passive: false});
     }
+
     const deleteBookMark = () => {
+      bookmarkRef.doc(delBookmarkTitle.value).delete()
+          .then(() => {
+            document.location.reload();
+          }).catch(error => {
+        console.log(error)
+      })
       hidePopup();
     }
 
